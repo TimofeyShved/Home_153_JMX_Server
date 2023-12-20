e9f6b6a0d9da6799aa25f36fff869d7e97b1afb4
TimofeyShved <timofeya123@inbox.ru>
2023-12-20 19:57:44 +0600

    Notification

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -1,14 +1,29 @@
 package com.company;
 
-public class Hello implements HelloMBean{
+import javax.management.*;
 
-    // данные с которыми работает сервер
+public class Hello extends NotificationBroadcasterSupport implements HelloMBean{
+
+    //----------------------------------------  данные с которыми работает сервер
 
     private String name = returnPerson().getName();
+    static int notificationNumber = 0;
 
     @Override
     public void sayHello() {
         System.out.println("Привет "+name+", это моя программа на JMX");
+
+        //работа с сообшениями на сервере(ниже есть нотификатор)
+        Notification notification = new AttributeChangeNotification(
+                this,
+                notificationNumber++,
+                System.currentTimeMillis(),
+                "Вызвали метод sayHello",
+                "Едиственное измение в нотификаторе переменной notificationNumber",
+                "int",
+                notificationNumber-1,
+                notificationNumber);
+        sendNotification(notification);
     }
 
     @Override
@@ -35,5 +50,27 @@ public class Hello implements HelloMBean{
     @Override
     public void setCacheSize(int size) {
 
+    }
+
+    // ---------------------------------------- Сообщения на сервере
+    // Кастомная информация при выполении кода на сервере
+    @Override
+    public MBeanNotificationInfo[] getNotificationInfo() {
+        // получаем тип того что меняем (наш атрибут)
+        String[] types = new String[]{
+                AttributeChangeNotification.ATTRIBUTE_CHANGE
+        };
+
+        // в каком классе
+        String className = AttributeChangeNotification.class.getName();
+        // сообщение
+        String description = "Изменение атрибута Hello";
+
+        //формируем и передаём сообщение на сервер
+        MBeanNotificationInfo info = new MBeanNotificationInfo(types, className, description);
+        return new  MBeanNotificationInfo[]{info};
+
+        // (по умолчанию было это)
+        // return super.getNotificationInfo();
     }
 }
