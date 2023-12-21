79f004e62f938bdfdbbe3a05cc51183baa532d35
TimofeyShved <timofeya123@inbox.ru>
2023-12-21 17:20:38 +0600

    Client update

--- src/com/company/client/Client.java
+++ src/com/company/client/Client.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class Client {
-    public static void main(String[] args) throws IOException, MalformedObjectNameException {
+    public static void main(String[] args) throws IOException, MalformedObjectNameException, InstanceNotFoundException, InterruptedException {
 
         // Подключаемся к нашему серверу по url
         JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://:9999/jmxrmi");
@@ -21,8 +21,13 @@ public class Client {
         ObjectName mbeanName = new ObjectName("com.company:type=Hello");
         HelloMBean mbeanProxy = JMX.newMBeanProxy(mbsc, mbeanName, HelloMBean.class, true);
 
+        // кроме взаимодействий нам ещё придёться добавить обработчик приходящих сообщений
+        mbsc.addNotificationListener(mbeanName, new ClientListener(), null, null);
+
         // Работаем с ним
         mbeanProxy.sayHello();
+        // спит текущий поток, что-бы потом обработать пришедший ответ (сообщение)
+        Thread.sleep(1000);
         System.out.println(mbeanProxy.add(1,2));
         System.out.println(mbeanProxy.getName());
         System.out.println(mbeanProxy.returnPerson().getName());
